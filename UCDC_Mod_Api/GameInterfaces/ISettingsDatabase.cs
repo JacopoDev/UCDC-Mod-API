e0f62513107c1a208912107c47c19f846b0a6ea0
jakub.strekowski <jakub.strekowski@plum.pl>
2025-06-05 19:54:56 +0200

    Add batch loading and saving settings

--- UCDC_Mod_Api/GameInterfaces/ISettingsDatabase.cs
+++ UCDC_Mod_Api/GameInterfaces/ISettingsDatabase.cs
@@ -1,9 +1,11 @@
-﻿using UMod;
+﻿using System.Collections.Generic;
+using UMod;
 
 namespace UCDC_Mod_Api.GameInterfaces
 {
     public interface ISettingsDatabase : IModPersistentData
     {
-        
+        Dictionary<string, object> LoadGroupData(string[] values);
+        void SaveGroupData(Dictionary<string, object> settings);
     }
 }
