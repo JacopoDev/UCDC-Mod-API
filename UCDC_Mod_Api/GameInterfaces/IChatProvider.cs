80a0fcab164778164fd6fb2efc57e1af2093f263
Jacopo <whydoIneedToPasteEmail@godohwhy.why>
2025-09-04 15:28:07 +0200

    Add output schema for text generation

--- UCDC_Mod_Api/GameInterfaces/IChatProvider.cs
+++ UCDC_Mod_Api/GameInterfaces/IChatProvider.cs
@@ -1,4 +1,5 @@
-﻿using UCDC_Mod_Api.Models;
+﻿using JetBrains.Annotations;
+using UCDC_Mod_Api.Models;
 using UCDC_Mod_Api.Models.TextGen;
 using UCDC_Mod_Api.ModInterfaces;
 
@@ -6,10 +7,12 @@ namespace UCDC_Mod_Api.GameInterfaces
 {
     /// <summary>
     /// Notice: Interface is implemented by the game classes <br /><br />
-    /// Gives current list of messages to be processed in text-gen API (chat messages, emotion analysis, user facts)
+    /// Gives current list of messages to be processed in text-gen API (chat messages, emotion analysis, user facts) <br />
+    /// <see cref="IChatProvider.GetResponseFormat"/> is used only for AI that has support for structured outputs, serialized in json with schema
     /// </summary>
     public interface IChatProvider
     {
         Chat GetChat();
+        [CanBeNull] string GetResponseFormat();
     }
 }
