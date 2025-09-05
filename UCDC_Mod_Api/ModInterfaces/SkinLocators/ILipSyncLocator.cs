00dfbfac63ad8b02949f5c3a2b3ae1954f53cf68
Jacopo <whydoIneedToPasteEmail@godohwhy.why>
2025-09-05 13:26:40 +0200

    Add lip sync interfaces

---
+++ UCDC_Mod_Api/ModInterfaces/SkinLocators/ILipSyncLocator.cs
@@ -0,0 +1,13 @@
+﻿using UCDC_Mod_Api.Models.Skins;
+
+namespace UCDC_Mod_Api.ModInterfaces.SkinLocators
+{
+    /// <summary>
+    /// Implement this if you want your skin to have lip sync when she speaks <br /><br />
+    /// <remarks> It is only used when there is any voice mod enabled and working. <br /> </remarks>
+    /// </summary>
+    public interface ILipSyncLocator
+    {
+        public LipSyncData GetLipSyncData();
+    }
+}
---
+++ UCDC_Mod_Api/Models/Skins/LipSyncData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace UCDC_Mod_Api.Models.Skins
+{
+    [Serializable]
+    public class LipSyncData
+    {
+        public SkinnedMeshRenderer lipSyncMesh;
+        public BlendShapeMorph a;
+        public BlendShapeMorph u;
+        public BlendShapeMorph i;
+        public BlendShapeMorph e;
+        public BlendShapeMorph o;
+        public BlendShapeMorph n;
+        public BlendShapeMorph none;
+    }
+}
